f487e2ba341057dc718a3099d5b79139b2fdf8cf
Tejshree Karande <tejshreekarande23@gmail.com>
2026-03-08 22:04:22 +0530

    Exp 1 to 5

---
+++ Exp_01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class ArithmeticOperations
+{
+    static void Main()
+    {
+        int a, b;
+
+        Console.WriteLine("Enter first number:");
+        a = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter second number:");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Addition: " + (a + b));
+        Console.WriteLine("Subtraction: " + (a - b));
+        Console.WriteLine("Multiplication: " + (a * b));
+        Console.WriteLine("Division: " + (a / b));
+        Console.WriteLine("Modulus: " + (a % b));
+    }
+}
